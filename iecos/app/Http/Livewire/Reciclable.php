c82eac7ca79890e2658b3e7ff579a918be6a92b4
ByAncort <91231741+ByAncort@users.noreply.github.com>
2024-05-23 11:07:24 -0400

    formula reciclaje

--- iecos/app/Http/Livewire/Reciclable.php
+++ iecos/app/Http/Livewire/Reciclable.php
@@ -34,52 +34,65 @@ class Reciclable extends Component
             'userId' => 'required|exists:users,id', 
         ]);
     
+        // Obtiene la información del usuario
         $user = DB::table('users')->where('id', $this->userId)->first();
+    
         if ($user) {
+            // Obtiene el nombre, correo electrónico y rol del usuario
             $this->userName = $user->name;
             $this->userEmail = $user->email;
             $this->userRoleId = $user->role_id;
     
+            // Obtiene la cantidad de reciclaje
             $quantity = intval($this->quantity);
+    
+            // Inicializa los puntos
             $puntos = 0;
-           
-                $puntos = 0; // Inicializa los puntos
-                if ($this->selectedTypeId == 1) {
-                    $puntos = $this->quantity / 2;
-                } elseif ($this->selectedTypeId == 2) {
-                    $puntos = $this->quantity / 3;
-                } elseif ($this->selectedTypeId == 3) {
-                    $puntos = $this->quantity / 1.5;
-                }
-
-
-
-                $puntaje = $user->puntos;
-                $nuevoPuntaje = $puntaje + $puntos;
-               
-                DB::table('users')->where('id', $this->userId)->update(['puntos' => $nuevoPuntaje]);
-            
+    
+            // Busca el puntaje por kilo correspondiente al tipo de reciclaje seleccionado
+            $puntaje = DB::table('puntajes')
+                        ->where('recycling_type_id', $this->selectedTypeId)
+                        ->first();
+    
+            if ($puntaje) {
+                // Calcula los puntos según la cantidad reciclada y el puntaje por kilo
+                $weight = intval($puntaje->weight);
+                $point=$puntaje->point;
+                $puntosxkg=$point/$weight;
+                $puntos = intval($quantity * $puntosxkg);
+                // dd($puntos);
+            } else {
+                // Si no se encuentra un puntaje por kilo para el tipo de reciclaje seleccionado, muestra un mensaje de error
+                session()->flash('error', 'No se encontró el puntaje correspondiente al tipo de reciclaje seleccionado');
+                return;
+            }
+    
+            // Actualiza el puntaje del usuario
+            $nuevoPuntaje = $user->puntos + $puntos;
+            DB::table('users')->where('id', $this->userId)->update(['puntos' => $nuevoPuntaje]);
     
             // Inserta el nuevo elemento reciclable
             DB::table('recyclable_items')->insert([
-                'quantity' => $this->quantity,
+                'quantity' => $quantity,
                 'recycling_type_id' => $this->selectedTypeId,
                 'user_id' => $this->userId,
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
+    
+            // Reinicia las variables
+            $this->quantity = null;
+            $this->userId = null;
+            $this->selectedTypeId = null;
+            $this->successMessage = true;
+            session()->flash('success', '¡El reciclaje se ha guardado correctamente!');
         } else {
+            // Si no se encuentra el usuario, muestra un mensaje de error
             session()->flash('error', 'Usuario no encontrado');
             return;
         }
-    
-        // Reinicia las variables
-        $this->quantity = null;
-        $this->userId = null;
-        $this->selectedTypeId = null;
-        $this->successMessage = true;
-        session()->flash('success', '¡El reciclaje se ha guardado correctamente!');
     }
+    
     
 
     public function updatedUserId()
--- iecos/resources/views/layouts/navbars/auth/sidebar.blade.php
+++ iecos/resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -143,7 +143,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link {{ Route::currentRouteName() == 'tables' ? 'active' : '' }}"
+                <a class="nav-link {{ Route::currentRouteName() == 'reciclable' ? 'active' : '' }}"
                     href="{{ route('reciclable') }}">
                     <div
                         class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
