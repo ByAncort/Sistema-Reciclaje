94ffb1a75a9a594deccdfa9defad58e9af360062
ByAncort <91231741+ByAncort@users.noreply.github.com>
2024-06-25 13:05:28 -0400

    info update

--- iecos/resources/views/welcomeV2.blade.php
+++ iecos/resources/views/welcomeV2.blade.php
@@ -73,7 +73,7 @@
             <img class="img-fluid2" src="{{ asset('assets/img/imgWelcomeV2/iphone2.png') }}" alt="..." />
         </div>
     </section>
-    <section class="about-section text-center" id="about-2">
+    <section class="about-section-top text-center" id="about-2">
         <div class="container px-4 px-lg-5">
             <div class="row gx-4 gx-lg-5 justify-content-center">
                 <div class="col-lg-8">
@@ -105,7 +105,7 @@
                         <p class="text-black-50 mb-0">Ser Socio Colaborador es muy sencillo. Solo debes inscribirte en
                             nuestra aplicación
                             <a href="#">xxxx</a>
-                            o en está página WEB.
+                            o en está <a href="{{ route('sign-up') }}">página WEB</a>.
                         </p>
                     </div>
                 </div>
@@ -195,14 +195,14 @@
     <!-- Contact-->
     <section class="contact-section bg-black">
         <div class="container px-4 px-lg-5">
-            <!-- <div class="row gx-4 gx-lg-5">
+            <div class="row gx-4 gx-lg-5">
                 <div class="col-md-4 mb-3 mb-md-0">
                     <div class="card py-4 h-100">
                         <div class="card-body text-center">
-                            <i class="fas fa-map-marked-alt text-primary mb-2"></i>
-                            <h4 class="text-uppercase m-0">Address</h4>
+                            <i class="fa-brands fa-google text-primary mb-2"></i>
+                            <h4 class="text-uppercase m-0">WebSite</h4>
                             <hr class="my-4 mx-auto" />
-                            <div class="small text-black-50">4923 Market Street, Orlando FL</div>
+                            <div class="small text-black-50"><a href="/">www.iecos.cl</a></div>
                         </div>
                     </div>
                 </div>
@@ -212,7 +212,7 @@
                             <i class="fas fa-envelope text-primary mb-2"></i>
                             <h4 class="text-uppercase m-0">Email</h4>
                             <hr class="my-4 mx-auto" />
-                            <div class="small text-black-50"><a href="#!">hello@yourdomain.com</a></div>
+                            <div class="small text-black-50"><a href="#!">sergioveloz@iecos.cl</a></div>
                         </div>
                     </div>
                 </div>
@@ -222,11 +222,11 @@
                             <i class="fas fa-mobile-alt text-primary mb-2"></i>
                             <h4 class="text-uppercase m-0">Phone</h4>
                             <hr class="my-4 mx-auto" />
-                            <div class="small text-black-50">+1 (555) 902-8832</div>
+                            <div class="small text-black-50">+56 9 8156 3517</div>
                         </div>
                     </div>
                 </div>
-            </div> -->
+            </div>
             <div class="social d-flex justify-content-center">
                 <a class="mx-2" href="#!"><i class="fab fa-twitter"></i></a>
                 <a class="mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
